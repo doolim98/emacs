6e4a00b69406d839c464687c35e381ed6214258b
hjoll6 <doolim98@gmail.com>
2023-06-23 15:25:02 +0900

    Remove esc as quit

--- my/emacs.el
+++ my/emacs.el
@@ -97,13 +97,5 @@ Version 2016-10-25"
    ;; mac-option-modifier 'meta
    ))
 
-;; esc always quits
-;; (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-;; (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-;; (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-;; (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-;; (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
-;; (global-set-key [escape] 'keyboard-quit)
-
 
 
